1bf7b3dc9203b431c03844478f7e991f79c9db6d
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-06-21 23:07:30 +0530

    section 2 completed here

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 const Header = () => {
     return (
@@ -11,21 +12,27 @@ const Header = () => {
 
                 <Container>
                     {/*  A Bootstrap component that provides a responsive fixed-width container */}
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="E-commerce" />
-                        E-Commerce
-                    </Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img src={logo} alt="E-commerce" />
+                            E-Commerce
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart">
+                            <LinkContainer to="/cart">
+                            <Nav.Link>
                                 <FaShoppingCart />
                                 Cart
                             </Nav.Link>
-                            <Nav.Link href="/login">
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                            <Nav.Link>
                                 <FaUser />
                                 Sign In
                             </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,22 +1,27 @@
 import Card from 'react-bootstrap/Card';
+import {Link} from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`./product/${product._id}`}>
+            <Link to={`./product/${product._id}`}>
                 <Card.Img src={product.image} variant="top" />
                 {/* Card.img is a sub-component of the Card component from the react-bootstrap library. It is specifically designed to handle the display of images within a card. */}
 
                 {/*  "top": Setting variant to "top" places the image at the top of the card. This is useful for cards where you want the image to be the first thing a user sees. */}
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${product._id}`}>
-                    <Card.Title as="div">
+                <Link to={`/product/${product._id}`}>
+                    <Card.Title as="div" className='product-title'>
                         <strong>{product.name}</strong>
                     </Card.Title>
 
                     {/* as="div": The as prop allows you to render the Card.Title component as a different HTML element. Here, it is being rendered as a <div> instead of the default <h5> element */}
-                </a>
+                </Link>
+                <Card.Text as='div'>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                </Card.Text>
 
                 <Card.Text as="h3">
                     ${product.price}
