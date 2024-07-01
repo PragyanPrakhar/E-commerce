3c39f1c00e6e7c1c197ec89483b03b3fbe0743ae
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-07-02 01:46:09 +0530

    cart functionality

---
+++ frontend/src/utils/cardUtils.js
@@ -0,0 +1,18 @@
+export const addDecimals=(num)=>{
+    return (Math.round(num*100)/100).toFixed(2);
+};
+
+export const updateCart=(state)=>{
+    state.itemsPrice = addDecimals(state.cartItems.reduce((acc,item)=>acc+item.price*item.qty,0));
+    //calculating shipping price(if Order is over $100 then free , else $10 order)
+
+    state.shippingPrice = addDecimals(state.itemsPrice>100 ?0:10);
+    //calculate tax price(15% tax)
+    state.taxPrice=addDecimals(Number(0.15*state.itemsPrice).toFixed(2));
+
+    //calculate total price
+    state.totalPrice = (Number(state.itemsPrice)+Number(state.shippingPrice)+Number(state.taxPrice)).toFixed(2);
+
+    localStorage.setItem('cart',JSON.stringify(state));
+    return state;
+}
